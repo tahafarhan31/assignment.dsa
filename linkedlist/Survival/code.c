4f7bf034e6fadfd06ef7b2b63e49dd944b12ba91
tahafarhan31 <120447563+tahafarhan31@users.noreply.github.com>
2023-09-10 21:14:06 +0500

    code.c

--- linkedlist/Survival/code.c
+++ linkedlist/Survival/code.c
@@ -2,57 +2,54 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-void insert(int arr[], int n, int data) {
-	
-    int i = n - 1;
-    while (i >= 0 && arr[i] > data) {
-        arr[i + 1] = arr[i];
+void insert_sorted(int array[], int size, int value) {
+    int i = size - 1;
+    while (i >= 0 && array[i] > value) {
+        array[i + 1] = array[i];
         i--;
     }
-    arr[i + 1] = data;
+    array[i + 1] = value;
 }
 
-double get_median(int arr[], int n) {
-    if (n % 2 == 0) {
-    
-        int mid1 = arr[n / 2 - 1];
-        int mid2 = arr[n / 2];
+double calculate_median(int array[], int size) {
+    if (size % 2 == 0) {
+        int mid1 = array[size / 2 - 1];
+        int mid2 = array[size / 2];
         return (double)(mid1 + mid2) / 2.0;
-        
     } else {
-        
-        return (double)arr[n / 2];
+        return (double)array[size / 2];
     }
 }
 
 int main() {
-    FILE* fp;
-    int data;
-    int* arr = NULL;
-    int size = 0;
+    FILE* file_pointer;
+    int value;
+    int* values_array = NULL;
+    int array_size = 0;
 
-    fp = fopen("input.txt", "r");
-    if (fp == NULL) {
-        printf("Error occurs while opening file\n");
+    file_pointer = fopen("input.txt", "r");
+    if (file_pointer == NULL) {
+        printf("Error opening the file\n");
         return 1;
     }
 
-    while (fscanf(fp, "%d", &data) != EOF) {
-        size++;
-        arr = (int*)realloc(arr, size * sizeof(int));
-        if (arr == NULL) {
-            printf("There is some error while allocating the memory error\n");
+    while (fscanf(file_pointer, "%d", &value) != EOF) {
+        array_size++;
+        values_array = (int*)realloc(values_array, array_size * sizeof(int));
+        if (values_array == NULL) {
+            printf("Error allocating memory\n");
             return 1;
         }
 
-        insert(arr, size - 1, data);
+        insert_sorted(values_array, array_size - 1, value);
 
-        double median = get_median(arr, size);
-        printf(" The Median of %d is  : %.1f\n", data, median);
+        double median = calculate_median(values_array, array_size);
+        printf("The Median of %d is: %.1f\n", value, median);
     }
 
-    free(arr);
-    fclose(fp);
+    free(values_array);
+    fclose(file_pointer);
 
     return 0;
 }
+
